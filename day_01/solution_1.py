0912734b82443719c1f537d9e00f40039eecb4a3
Jim Lawton <jim.lawton@gmail.com>
2025-12-05 15:50:22 +0000

    Split solve logic out of main. Add tester.

--- day_01/solution_1.py
+++ day_01/solution_1.py
@@ -11,19 +11,7 @@ INITIAL = 50
 SIZE = 100
 
 
-def main():
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <file>")
-        sys.exit(1)
-
-    file = sys.argv[1]
-    if not os.path.exists(file):
-        print(f"File not found: {file}")
-        sys.exit(1)
-
-    with open(file, "r") as f:
-        lines = f.readlines()
-
+def solve(lines):
     num_zeroes = 0
     current = INITIAL
     for rotation in lines:
@@ -38,7 +26,54 @@ def main():
         if new == 0:
             num_zeroes += 1
         current = new
+    return current, num_zeroes
 
+
+def test():
+    fails = 0
+    print("\nRunning tests #1...")
+    test_data = [["L50", "R50"], ["L50", "L50"], ["R50", "L50"], ["R50", "R50"]]
+    for i, test_case in enumerate(test_data):
+        print(f"  Test case {i + 1}:")
+        current, num_zeroes = solve(test_case)
+        if num_zeroes == 1:
+            print("  PASS")
+        else:
+            fails += 1
+            print(f"  FAIL: expected 1 zero hit, got {num_zeroes}")
+
+    print("\nRunning tests #2...")
+    test_data = [["L150", "L50"], ["L150", "R50"], ["R150", "L50"], ["R150", "R50"]]
+    for i, test_case in enumerate(test_data):
+        print(f"  Test case {i + 1}:")
+        current, num_zeroes = solve(test_case)
+        if num_zeroes == 1:
+            print("  PASS")
+        else:
+            fails += 1
+            print(f"  FAIL: expected 1 zero hit, got {num_zeroes}.")
+
+    if fails == 0:
+        print("\nAll tests PASS.")
+    else:
+        print(f"\n{fails} tests FAIL.")
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <file>")
+        test()
+        sys.exit(0)
+
+    file = sys.argv[1]
+    if not os.path.exists(file):
+        print(f"File not found: {file}")
+        sys.exit(1)
+
+    with open(file, "r") as f:
+        lines = f.readlines()
+
+    current, num_zeroes = solve(lines)
     print(f"\nNumber of times dial hit zero: {num_zeroes}")
 
 
--- day_01/solution_2.py
+++ day_01/solution_2.py
@@ -11,20 +11,8 @@ INITIAL = 50
 SIZE = 100
 
 
-def main():
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <file>")
-        sys.exit(1)
-
-    file = sys.argv[1]
-    if not os.path.exists(file):
-        print(f"File not found: {file}")
-        sys.exit(1)
-
-    with open(file, "r") as f:
-        lines = f.readlines()
-
-    num_zeroes = 0
+def solve(lines):
+    zero_hits = 0
     current = INITIAL
     zero_crossings = 0
     print(f"Dial starts at position {current}")
@@ -56,14 +44,66 @@ def main():
 
         new = (current + steps) % SIZE
         if new == 0:
-            num_zeroes += 1
+            zero_hits += 1
 
         # print(f"Rotated {rotation} from {current} to point at {new}")
         current = new
+    return current, zero_hits, zero_crossings
 
-    print(f"\nNumber of times dial hit zero: {num_zeroes}")
+
+def test():
+    fails = 0
+
+    print("\nRunning tests #1...")
+    test_data = [["L50", "R50"], ["L50", "L50"], ["R50", "L50"], ["R50", "R50"]]
+    for i, test_case in enumerate(test_data):
+        print(f"  Test case {i + 1}:")
+        current, zero_hits, zero_crossings = solve(test_case)
+        if zero_hits == 1 and zero_crossings == 0:
+            print("  PASS")
+        else:
+            fails += 1
+            print(
+                f"  FAIL: Test case expected 1 zero hit and no zero crossing, got {zero_hits}, {zero_crossings}"
+            )
+
+    print("\nRunning tests #2...")
+    test_data = [["L150", "L50"], ["L150", "R50"], ["R150", "L50"], ["R150", "R50"]]
+    for i, test_case in enumerate(test_data):
+        print(f"  Test case {i + 1}:")
+        current, zero_hits, zero_crossings = solve(test_case)
+        if zero_hits == 1 and zero_crossings == 1:
+            print("  PASS")
+        else:
+            fails += 1
+            print(
+                f"  FAIL: expected 1 zero hit and 1 zero crossing, got {zero_hits}, {zero_crossings}"
+            )
+
+    if fails == 0:
+        print("\nAll tests PASS.")
+    else:
+        print(f"\n{fails} tests FAIL.")
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <file>")
+        test()
+        sys.exit(0)
+
+    file = sys.argv[1]
+    if not os.path.exists(file):
+        print(f"File not found: {file}")
+        sys.exit(1)
+
+    with open(file, "r") as f:
+        lines = f.readlines()
+
+    current, zero_hits, zero_crossings = solve(lines)
+    print(f"\nNumber of times dial hit zero: {zero_hits}")
     print(f"Number of times dial crossed zero: {zero_crossings}")
-    print(f"Total: {num_zeroes + zero_crossings}")
+    print(f"Total: {zero_hits + zero_crossings}")
 
 
 if __name__ == "__main__":
