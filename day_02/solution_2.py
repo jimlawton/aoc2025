1fbe3d1640c4bdd07dca32a7c25e918815b914f6
Jim Lawton <jim.lawton@gmail.com>
2025-12-08 20:56:05 +0000

    Day 2 part 2 solution.

---
+++ day_02/solution_2.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+def solve(lines):
+    repeats = []
+    for line in lines:
+        id_ranges = line.strip().split(",")
+        for id_range in id_ranges:
+            start_str, end_str = id_range.split("-")
+            start = int(start_str)
+            end = int(end_str)
+            range_ids = list(range(start, end + 1))
+            for id in range_ids:
+                str_id = str(id)
+                if len(str_id) < 2:
+                    continue
+                if len(str_id) % 2 != 0:
+                    # Odd length, must be all one digit.
+                    if str_id[:] == f"{str_id[0] * len(str_id)}":
+                        repeats.append(id)
+                        continue
+                    if len(str_id) % 3 == 0:
+                        third_len = len(str_id) // 3
+                        for part_len in range(1, third_len + 1):
+                            num_repeats = len(str_id) // part_len
+                            part = str_id[0:part_len]
+                            repeat_str = part * num_repeats
+                            # print(
+                            #     f"{len(str_id)=} {third_len=} {part_len=} {num_repeats=} {part=} {repeat_str=}"
+                            # ) if id == 824824824 else None
+                            if part * num_repeats == str_id:
+                                repeats.append(id)
+                                break
+                else:
+                    # Even length...
+                    half_len = len(str_id) // 2
+                    for part_len in range(1, half_len + 1):
+                        num_repeats = len(str_id) // part_len
+                        part = str_id[0:part_len]
+                        repeat_str = part * num_repeats
+                        print(
+                            f"{len(str_id)=} {half_len=} {part_len=} {num_repeats=} {part=} {repeat_str=}"
+                        ) if id == 824824824 else None
+                        if part * num_repeats == str_id:
+                            repeats.append(id)
+                            break
+    return repeats
+
+
+def test():
+    fails = 0
+    print("\nRunning tests #1...")
+    test_data = [
+        "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+    ]
+    for i, test_case in enumerate(test_data):
+        print(f"  Test case {i + 1}:")
+        repeats = solve([test_case])
+        expected_repeats = [
+            11,
+            22,
+            99,
+            111,
+            999,
+            1010,
+            1188511885,
+            222222,
+            446446,
+            38593859,
+            565656,
+            824824824,
+            2121212121,
+        ]
+        if repeats == expected_repeats:
+            print("    PASS")
+        else:
+            print(
+                f"    FAIL: expected {expected_repeats}, \n               got {repeats}"
+            )
+            fails += 1
+        if sum(repeats) == sum(expected_repeats):
+            print("    PASS sum")
+        else:
+            print(f"    FAIL: expected sum {sum(expected_repeats)}, got {sum(repeats)}")
+            fails += 1
+
+    if fails == 0:
+        print("\nAll tests PASS.")
+    else:
+        print(f"\n{fails} tests FAIL.")
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <file>")
+        test()
+        sys.exit(0)
+
+    file = sys.argv[1]
+    if not os.path.exists(file):
+        print(f"File not found: {file}")
+        sys.exit(1)
+
+    with open(file, "r") as f:
+        lines = f.readlines()
+
+    repeats = solve(lines)
+    print(f"Found {len(repeats)} repeated IDs: {repeats}")
+    print(f"Sum of repeated IDs: {sum(repeats)}")
+
+
+if __name__ == "__main__":
+    main()
